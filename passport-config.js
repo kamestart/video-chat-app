c268e568c83b1a67819147588ee258ea5cc46f80
kamestart <ajmeramanit230734@gmail.com>
2020-09-21 16:50:40 +0530

    first commit

--- passport-config.js
+++ passport-config.js
@@ -1,28 +1,32 @@
-// const localStrat = require('passport-local').Strategy
-//const bcrypt = require('bcrypt')
+const localStrat = require('passport-local').Strategy
+const bcrypt = require('bcrypt')
+const passport = require('passport')
 
 
-// async function initializePassportLocal(passport, getUserByEmail) {
- //   const authenticateUser = (username, password, done) => {
-   //     const user = getUserByUsername(username)
-     //   if (user == null) {
-       //     return done(null, false, 'Incorrect Username Or Password ')
-        //}
-//    }
+function initializePassportLocal(passport, getUserByUsername) {
+    const authenticateUser = async (username, password, done) => {
+        const user = getUserByUsername(username)
+        if (user == null) {
+            return done(null, false, { message: 'Incorrect Username Or Password ' })
+        }
+    
 
-  //  try {
-    //    if (await bcrypt.compare(password, user.password)) {
-      //      return done(null, user)
-        // } else {
-           // return done(null, false, 'Incorrect Username Or Password ')
-        // }
-    // } catch (e) {
-       //  return done(e)
-     //}
-    //passport.use(new localStrat({ usernameField: 'username' }),
-    //authenticateUser)
-    //passport.serializeUser((user, done) => {  })
-    //passport.deserializeUser((id, done) => {  })
-//}
+    try {
+        if (await bcrypt.compare(password, user.password)) {
+            return done(null, user)
+        } else {
+            return done(null, false, {message: 'Incorrect Username Or Password '})
+        }
+    } catch (e) {
+         return done(e)
+    }
+    }
+   
 
-// module.exports = initializePassportLocal
+  passport.use(new localStrat({ usernameField: 'username' },
+  authenticateUser))
+  passport.serializeUser((user, done) => {  })
+  passport.deserializeUser((id, done) => {  })
+}
+
+module.exports = initializePassportLocal
--- server.js
+++ server.js
@@ -9,7 +9,12 @@ const Bu = require('./models/users')
 const bcrypt = require('bcrypt')
 const initPassport = require('./passport-config')
 const passport = require('passport')
-//initPassport(passport)
+const flash = require('express-flash')
+const session = require('express-session')
+
+initPassport(passport, username => {
+    Bu.find(user => {username: username})
+})
 
 app.listen(PORT, () => {
     console.log(`Server Started on port ${PORT} `)
@@ -17,13 +22,21 @@ app.listen(PORT, () => {
 
 app.use(express.static(__dirname + '/public'))
 app.use(express.urlencoded({ extended: false }))
+app.use(flash())
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
 
 mongoose.connect(process.env.DATABASE_URL, { useUnifiedTopology: true, useNewUrlParser: true })
 
 app.set('view-engine', 'ejs')
 
 app.get('/', (req, res) => {
-    res.render('index.ejs')
+    res.render('register.ejs')
 })
 
 app.get('/login', (req, res) => {
@@ -32,11 +45,13 @@ app.get('/login', (req, res) => {
 
 const db = mongoose.connection
 
-app.post('/login', (req, res) => {
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/home',
+    failureRedirect: '/login',
+    failureFlash: true
+}))
 
-})
-
-app.post('/register', async (req, res) => {
+app.post('/', async (req, res) => {
     let i = 1
     try {
         const hashedPwd = await bcrypt.hash(req.body.password, 15)
@@ -54,6 +69,6 @@ app.post('/register', async (req, res) => {
     }
 })
 
-app.get('/register', (req, res) => {
-    res.render('register.ejs')
+app.get('/home', (req, res) => {
+    res.render('index.ejs')
 })
