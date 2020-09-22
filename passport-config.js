fb8d531654717ccf5cb8dccea634209845d77532
kamestart <ajmeramanit230734@gmail.com>
2020-09-22 12:00:11 +0530

    test commit 15

--- passport-config.js
+++ passport-config.js
@@ -19,11 +19,15 @@ function initialize(passport, getUserByEmail, getUserById) {
     }
   }
 
-  passport.use(new LocalStrategy({ usernameField: 'username', passwordField: 'password' }, authenticateUser))
+  passport.use(new LocalStrategy({
+    usernameField: 'username',
+    passwordField: 'password' 
+  }, authenticateUser))
+
   passport.serializeUser((user, done) => done(null, user.id))
   passport.deserializeUser((id, done) => {
     return done(null, getUserById(id))
   })
 }
 
-module.exports = initialize
+module.exports = initialize()
