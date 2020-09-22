2291f8cdb7ca0093f792147a7e3c469b60ffcd03
kamestart <ajmeramanit230734@gmail.com>
2020-09-22 11:09:57 +0530

    test commit 8

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.listen(PORT, () => {
 })
 
 app.use(express.static(__dirname + '/public'))
-app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 app.use(flash())
 app.use(session({
     cookie: {
