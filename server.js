4fb16928dd139625dc2d9855065b62fddd01330d
kamestart <ajmeramanit230734@gmail.com>
2020-09-27 16:23:20 +0530

    heroku app V24

--- server.js
+++ server.js
@@ -14,10 +14,10 @@ const session = require('express-session')
 var MemoryStore = require('memorystore')(session)
 const bodyParser = require('body-parser')
 
-const find_user_by_username = (username2) => user = User.find( { username: username2 } )
+const find_user_by_username = (username2) => user = process.env.DATABASE_URL.JustDB.users.find( { username: username2 } )
 
 
-const find_user_password = (username3) => user_password =  User.find({ username: username3 }, `password`).limit(1)   
+const find_user_password = (username3) => user_password =  process.env.DATABASE_URL.JustDB.users.find({ username: username3 }, `password`).limit(1)   
  
 
 const find_user_by_id = id2 => user = User.findById(id2)
